28f07df0cd1db24a7eb0b13a799f22a4456f065f
tfalque.ext <thibault.falque@exakis-nelite.com>
2021-05-04 14:46:06 +0200

    v0.1.0rc5

--- autograph/__init__.py
+++ autograph/__init__.py
@@ -48,7 +48,7 @@ __title__ = 'autograph'
 __summary__ = 'AUtogRAPH - A Unified libRary for drAwing Plots in pytHon'
 __keywords__ = 'visualization plots'
 __uri__ = 'https://github.com/crillab/autograph'
-__version__ = '0.1.0-rc4'
+__version__ = '0.1.0-rc5'
 __author__ = 'Thibault Falque, Romain Wallon, Hugues Wattez'
 __email__ = 'thibault.falque@exakis-nelite.com, wallon@lix.polytechnique.fr, wattez@cril.fr'
 
