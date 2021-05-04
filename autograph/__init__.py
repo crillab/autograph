72f6158828c7874e3ad63f930401b6282aa46f00
tfalque.ext <thibault.falque@exakis-nelite.com>
2021-05-04 16:53:20 +0200

    :ambulance: fixes globals => locals

--- autograph/__init__.py
+++ autograph/__init__.py
@@ -32,7 +32,7 @@ def create_plot(name: str):
 
     for elt in Plot.__subclasses__():
         if elt.name == name.lower():
-            return globals()[elt.__name__]()
+            return locals()[elt.__name__]()
     raise ValueError(f'{name} is not a valid class')
 
 
